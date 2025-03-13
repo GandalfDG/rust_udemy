510d839afbf620f1d28785e87bc75205428a8f10
Jack Case <jackacase@gmail.com>
2025-03-13 20:50:31 +0000

    oh no! A bug!

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -3,6 +3,12 @@ struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
 #[derive(Debug)]
 struct Account {
     balance: i32,
@@ -10,6 +16,21 @@ struct Account {
     holder: String,
 }
 
+impl Account {
+    fn new(holder_name: String, account_id: u32) -> Self {
+        Account { holder: holder_name, id: account_id, balance: 0 }
+    }
+}
+
+fn print_account(account: Account) {
+    println!("{account:#?}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let account = Account::new(String::from("Steve"), 123);
+
+    println!("{bank:#?}");
+    print_account(account);
+    print_account(account);
 }
