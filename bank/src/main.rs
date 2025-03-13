0983d4b62e7300ef66c048ef00d70131d80b50f9
Jack Case <jackacase@gmail.com>
2025-03-13 20:41:23 +0000

    initial bank and account structs

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+#[derive(Debug)]
+struct Account {
+    balance: i32,
+    id: u32,
+    holder: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
