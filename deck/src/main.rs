f6cc524412839ed6e980b93392a4965aa5dfec70
Jack Case <jackacase@gmail.com>
2025-03-13 19:17:38 +0000

    generate a deck of card names with a nested loop 2.10

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // list of suits
+    let suits = ["hearts", "diamonds", "clubs", "spades"];
+    let values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+    
+    let deck: Deck = Deck { cards };
+
+    println!("Here's your deck: {deck:#?}");
 }
