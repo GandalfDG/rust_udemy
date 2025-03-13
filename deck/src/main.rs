1e991ebb572ebaec11885ce84cb1ea9ee547a007
Jack Case <jackacase@gmail.com>
2025-03-13 19:29:46 +0000

    pull new deck creation out of main and into Deck impl 2.12

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -3,21 +3,27 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // list of suits
-    let suits = ["hearts", "diamonds", "clubs", "spades"];
-    let values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
+impl Deck {
+    fn new() -> Self {
+        // list of suits
+        let suits = ["hearts", "diamonds", "clubs", "spades"];
+        let values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+        
+        Deck { cards }
     }
-    
-    let deck: Deck = Deck { cards };
+}
+
+fn main() {
+    let deck = Deck::new();
 
     println!("Here's your deck: {deck:#?}");
 }
